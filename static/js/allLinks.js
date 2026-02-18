f0396e7ba8d1ba6d8b4e20b05deab15df5c8c18c
abc54882025 <abc54882025@gmail.com>
2026-02-18 15:28:10 +0800

    Update allLinks.js

--- static/js/allLinks.js
+++ static/js/allLinks.js
@@ -1,7 +1,7 @@
     const allLinks = [
                     "https://www.90265486.com",
                     "https://www.74hi77.com",
-                    "https://www75hi77.com",
+                    "https://www.75hi77.com",
                     "https://www.76hi77.com",
                     "https://www.777hi77.com",                   
         ];
