8d024db9ef24d2d34e95cbb7e868a3581a66e00f
abc54882025 <abc54882025@gmail.com>
2026-02-20 23:20:10 +0800

    Update allLinks.js

--- static/js/allLinks.js
+++ static/js/allLinks.js
@@ -1,7 +1,7 @@
     const allLinks = [
-                    "https://www.90265486.com",
                     "https://www.hi77.games",
-                    "https://www.75hi77.com",
-                    "https://www.76hi77.com",
-                    "https://www.777hi77.com",                   
+                    "https://www.hi77.games",
+                    "https://www.hi77.games",
+                    "https://www.hi77.games",
+                    "https://www.hi77.games",                   
         ];
